69ff00a5a432b9737a545866f474109236806b7e
AitsuYuyu <136203413+AitsuYuyu@users.noreply.github.com>
2023-09-20 07:31:49 -0500

    feat: :memo: actualizacion del proceso del taller

--- main.js
+++ main.js
@@ -1,28 +1,33 @@
 let myfrom = document.querySelector("form");
 let myTbala = document.querySelector("#myData");
-addEventListener("DOMContentLoaded", async()=>{
+addEventListener("DOMContentLoaded", async () => {
     let res = await (await fetch("https://6509d043f6553137159c1059.mockapi.io/tabla")).json();
-    for (let i = 0; i < res.length; i++){
-        myTbala.insertAdjacentElement("beforeendd", ` 
-        <tr>
-            <td>${res[i].id}</td>
-            <td>${res[i].valor}</td>
-            <td>${res[i].caja}</td>
-        </tr>
-        `);
+    for (let i = 0; i < res.length; i++) {
+        const newRow = document.createElement("tr");
+        newRow.insertAdjacentElement = `
+        <td>${res[i].id}</td>
+        <td>${res[i].valor}</td>
+        <td>${res[i].caja}</td>
+    `;
+
+        myTbala.appendChild (newRow);
     }
 })
 
-myfrom.addEventListener("submit", (e) => {
+myfrom.addEventListener("submit", async (e) => {
     e.preventDefault();
-    const data =Object.fromEntries(new FormData(e.target));
-    const {valor} = data
+    const data = Object.fromEntries(new FormData(e.target));
+    const { valor } = data
     data.valor = (typeof valor === "string") ? Number(valor) : null;
     let config = {
         method: "POST",
-        headers:{"content-type": "applicarion/json"},
+        headers: { "content-type": "application/json" },
         body: JSON.stringify(data)
     };
-    let res = await (fetch ("https://6509d043f6553137159c1059.mockapi.io/tabla", config)).json();
+    let res = await (await fetch("https://6509d043f6553137159c1059.mockapi.io/tabla", config)).json();
     console.log(res);
 })
+
+
+
+
